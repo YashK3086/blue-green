1c25a6138af408424ff3c0ccbf7fd183874708eb
Yashvardhan Khanna <yashvardhankhanna360@gmail.com>
2026-03-22 02:11:08 +0530

    Initial lean deploy for automation

---
+++ terraform/main.tf
@@ -0,0 +1,54 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0" # Pins to version 5.x to avoid version 6.0 bugs
+    }
+  }
+}
+
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+
+data "aws_availability_zones" "available" {}
+
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.0.0"
+
+  name = "blue-green-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = slice(data.aws_availability_zones.available.names, 0, 3)
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+}
+
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "19.15.3"
+
+  cluster_name    = "blue-green-cluster"
+  cluster_version = "1.31"
+
+  vpc_id                         = module.vpc.vpc_id
+  subnet_ids                     = module.vpc.private_subnets
+  cluster_endpoint_public_access = true
+
+  eks_managed_node_groups = {
+    nodes = {
+      min_size     = 2
+      max_size     = 3
+      desired_size = 2
+      instance_types = ["t3.micro"]
+    }
+  }
+}
